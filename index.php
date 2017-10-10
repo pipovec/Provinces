372bd05bca3acaad13e26abe836906c7008c9764
pipovec <pipovec@users.noreply.github.com>
2017-10-10 07:53:21 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+  .
+  .
+  .
+<script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
+  .
+  .
+</head>
+
+<body>
+  .
+  .
+  .
+  <div id="clanProvince" style="font-size: 10px"></div>
+  .
+  <script>
+  /*  
+  *   Kod zo suboru provinces.js umiestnite medzi tieto dve znacky.
+  *   Zmenite si tam akurat uplne naspodu clan_id.
+  *   Clan_id pre svoj klan najdete v zozname klanov na https://fpcstat.cz/cz-sk-klany/  *
+  */
+  </script>
+  .
+</body>
+</html>
